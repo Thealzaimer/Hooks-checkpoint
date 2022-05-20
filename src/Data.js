1cb0d38899cf578a1575b560497db421296a3be3
mehdi cherif <mehdichrif0@gmail.com>
2022-05-20 22:36:56 +0100

    first commit

---
+++ src/Data.js
@@ -0,0 +1,63 @@
+export const Data = [
+    {
+        id: 1,
+        title: "Les Evadés",
+        rate: 4,
+        description:
+            "L'amitié d'un jeune banquier condamné à la prison à vie pour le meurtre de sa femme et d'un vétéran de la prison de Shawshank, le pénitencier le plus sévère de l'Etat du Maine.",
+        posterUrl:
+            "https://fr.web.img6.acsta.net/c_310_420/medias/nmedia/18/63/30/68/18686447.jpg",
+        
+    },
+
+    {
+        id: 2,
+        title: "The Dark Knight, Le Chevalier Noir",
+        rate: 1,
+        description:
+            "Batman entreprend de démanteler les dernières organisations criminelles de Gotham. Mais il se heurte bientôt à un nouveau génie du crime qui répand la terreur et le chaos dans la ville : le Joker...",
+        posterUrl:
+            "https://fr.web.img6.acsta.net/c_310_420/medias/nmedia/18/63/97/89/18949761.jpg",
+        
+    },
+    {
+        id: 3,
+        title: "Le Seigneur des anneaux : le retour du roi",
+        rate: 3,
+        description:
+            "Tandis que les ténèbres se répandent sur la Terre du Milieu, Aragorn se révèle être l'héritier caché des rois antiques. Quant à Frodon, toujours tenté par l'Anneau, il voyage à travers les contrées ennemies, se reposant sur Sam et Gollum...",
+        posterUrl:
+            "https://fr.web.img6.acsta.net/c_310_420/medias/nmedia/18/35/14/33/18366630.jpg",
+        
+    },
+    {
+        id: 4,
+        title: "Le Parrain",
+        rate: 3,
+        description:
+            "En 1945, à New York, les Corleone sont une des cinq familles de la mafia. Don Vito Corleone marie sa fille à un bookmaker. Sollozzo, parrain de la famille Tattaglia, propose à Don Vito une association dans le trafic de drogue...",
+        posterUrl:
+            "https://fr.web.img6.acsta.net/c_310_420/pictures/22/01/14/08/39/1848157.jpg",
+        
+    },
+    {
+        id: 5,
+        title: "La Ligne verte",
+        rate: 4,
+        description:
+            "Paul Edgecomb, Gardien-chef du pénitencier de Cold Mountain en 1935, était chargé de veiller au bon déroulement des exécutions capitales. Parmi les prisonniers se trouvait un colosse du nom de John Coffey...",
+        posterUrl:
+            "https://fr.web.img2.acsta.net/c_310_420/medias/nmedia/18/66/15/78/19254683.jpg",
+        
+    },
+    {
+        id: 6,
+        title: "Il était une fois dans l'Ouest",
+        rate: 4,
+        description:
+            "Un mystérieux desperado pourchasse sans relâche un dangereux criminel.",
+        posterUrl:
+            "https://fr.web.img3.acsta.net/c_310_420/pictures/16/07/19/17/05/434841.jpg",
+        
+    }
+];
